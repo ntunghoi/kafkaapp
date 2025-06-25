ab5ac5d26117b18f08e86cf6da63602068225192
ntunghoi <ntunghoi@gmail.com>
2025-06-25 22:04:52 +0800

    Add Spring Boot to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,29 @@
 plugins {
     id("java")
+    alias(libs.plugins.springframework.boot)
+    alias(libs.plugins.spring.dependency.management)
 }
 
 group = "com.ntunghoi"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    runtimeOnly(libs.h2)
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
